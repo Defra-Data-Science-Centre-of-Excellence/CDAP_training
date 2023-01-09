9a31d5f77c536780b686508ef7913a4b7126f043
rbolam <rike.bolam@gmail.com>
2023-01-09 09:52:21 +0000

    Adding filter and select

--- RStudio_in_CDAP/welcome_session.R
+++ RStudio_in_CDAP/welcome_session.R
@@ -13,10 +13,16 @@ library(readr)
 library(ggplot2)
 
 
+
+##-------------------------------Loading data ----------------------------------------------####
+
 ## We can load data into RStudio using read_csv():
 penguins <- readr::read_csv("/dbfs/mnt/lab/unrestricted/R_training/penguins.csv")
 penguins_raw <- read_csv("/dbfs/mnt/lab/unrestricted/R_training/penguins_raw.csv")
 
+
+
+##-------------------------------Investigating data ----------------------------------------####
 
 ## We can look at the whole dataset using View():
 View(penguins)
@@ -27,8 +33,26 @@ summary(penguins)
 ## We can look at the structure of the dataset (incl types of variables) using str():
 str(penguins)
 
-## we can also look at individual variables, for example using count():
+## We can also look at individual variables, for example using count():
 count(penguins, species)
+
+
+
+##-------------------------------Filtering (by row) and selecting (by column) --------------####
+
+## We can retain only those observations where species is Adelie, using filter:
+adelie <- filter(penguins, species == "Adelie")
+
+## We can check that all species in the new dataset are Adelie penguins:
+count(adelie, species)
+
+## We can retain or remove different columns using select():
+
+adelie_year <- select(adelie, species, year)
+
+adelie_no_year <- select(adelie, -year)
+
+
 
 
 ## To make plots in R, ggplot2 is a popular package:
